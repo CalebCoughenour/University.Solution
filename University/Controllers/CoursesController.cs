d1c7c487c2c62000da2d6c38a8c0a56e2c791c7d
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-24 15:53:06 -0700

    update views.

--- University/Controllers/CoursesController.cs
+++ University/Controllers/CoursesController.cs
@@ -48,7 +48,7 @@ namespace University.Controllers
       public ActionResult Edit(int id)
       {
         var thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
-        ViewBag.DepartmentId = new SelectList(_db.Departments, "DepartmentID", "Name");
+        ViewBag.DepartmentId = new SelectList(_db.Departments, "DepartmentId", "Name");
         return View(thisCourse);
       }
 
